daef50d4c73393888823761d327efd22cfb922e9
somnath46 <4684.somnath@gmail.com>
2021-06-16 19:34:08 +0530

    Initial project setup #1

---
+++ src/main/java/com/jt/order/JtOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jt.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtOrderServiceApplication.class, args);
+	}
+
+}
